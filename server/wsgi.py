90c2723a561cb5bacf7f70456ea9881c18adadd0
Luca Gobber <mnbrain@Samsung-Book-Ultra.local>
2025-12-18 18:16:03 +0100

    light correction

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 # 1. Point to your actual server folder
-project_home = '/home/mncompany/mysite/ulrTrack/server'
+project_home = '/home/mncompany/mysite/server'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 # 2. Switch to that directory so .env and database are found
