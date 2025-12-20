105ee2000d11f7d77b4e3cf0f8233f5737ccbe56
Luca Gobber <mnbrain@Samsung-Book-Ultra.local>
2025-12-20 17:33:40 +0100

    IDK

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,10 +1,9 @@
 import sys
 import os
-# 1. Point to your actual server folder
-project_home = '/home/mncompany/mysite/server'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-# 2. Switch to that directory so .env and database are found
-os.chdir(project_home)
-# 3. Import your app (flask_app.py)
-from flask_app import app as application
+# Aggiungi il percorso del progetto
+path = '/home/mncompany/ulrTrack'
+if path not in sys.path:
+    sys.path.append(path)
+# IMPORTANTE: Carica la nuova App Factory
+from server import create_app
+application = create_app()
